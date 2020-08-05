48de1eb3db53c413929573f730e76bfc2c8da753
maaiyedun <44991916+maaiyedun@users.noreply.github.com>
2020-08-05 03:27:05 -0500

    Update LRU.cpp

--- LRU.cpp
+++ LRU.cpp
@@ -1,88 +1,85 @@
-#include<bits/stdc++.h>
+ // LRU page replacement VS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+#include <map>
+
+
+
 using namespace std;
-int main(){
-	int cSize,pSize,i,j,k,hit=0;
-	cout<<"Enter the cache Size"<< endl;
-	cin>>cSize;
 
-	cout<<"Enter the number of Processes"<< endl;
-	cin>>pSize;
 
-	vector<int> p(pSize);
-	vector<int> hi(pSize);
+int main() {
+	int n, m, i, j, k, hit = 0;
+	cout << "Enter the cache Size" << endl;
+	cin >> n; 
 
-	cout<<"Enter Processes"<< endl;
-	for(i=0;i<pSize;i++){
-		cin>>p[i];
+	ifstream myfile("multi1.trc");
+	while (std::getline(myfile, line)) {
+		++m;
 	}
-	vector<vector<int>> a(cSize);
-	for(i=0;i<cSize;i++){
-		a[i]=vector<int>(pSize,-1);
+		myfile.close();
+	}
+
+	vector<int> p(m);
+	vector<int> hi(m);
+
+	ifstream Cfile("multi1.trc");
+	if (Cfile.is_open()) {
+	for (i = 0; i < m; i++) {
+		getline(Cfile, line);
+		 p[i]=line;
+	}		
+		Cfile.close();
+	}
+
+	vector<vector<int>> a(n);
+	for (i = 0; i < n; i++) {
+		a[i] = vector<int>(m, -1);
 	}
 	map <int, int> mp;
-	for(i=0;i<pSize;i++){
-		vector<pair<int,int>> c;
-		for(auto q: mp){
-			c.push_back({q.second,q.first});
+	for (i = 0; i < m; i++) {
+		vector<pair<int, int>> c;
+		for (auto q : mp) {
+			c.push_back({ q.second,q.first });
 		}
-		sort(c.begin(),c.end());
-		bool hasrun=false;
-		for(j=0;j<cSize;j++){
-			if(a[j][i]==p[i]){ 
+		sort(c.begin(), c.end());
+		bool hasrun = false;
+		for (j = 0; j < n; j++) {
+			if (a[j][i] == p[i]) {
 				hit++;
-				hi[i]=1;
-				mp[p[i]]=1;
-				hasrun=true;
+				hi[i] = 1;
+				mp[p[i]] = 1;
+				hasrun = true;
 				break;
 			}
-			if(a[j][i]==-1){
-				for(k=i;k<pSize;k++)
-					a[j][k]=p[i];
+			if (a[j][i] == -1) {
+				for (k = i; k < m; k++)
+					a[j][k] = p[i];
 				mp[p[i]]++;
-				hasrun=true;
+				hasrun = true;
 				break;
 			}
 		}
-		if(j==cSize||hasrun==false){
-			for(j=0;j<cSize;j++){
-				if(a[j][i]==c[c.size()-1].second){
+		if (j == n || hasrun == false) {
+			for (j = 0; j < n; j++) {
+				if (a[j][i] == c[c.size() - 1].second) {
 					mp.erase(a[j][i]);
-					for(k=i;k<pSize;k++)
-						a[j][k]=p[i];
+					for (k = i; k < m; k++)
+						a[j][k] = p[i];
 					mp[p[i]]++;
 					break;
 				}
 			}
 		}
-		for(auto q:mp){
-			if(q.first!=p[i]){
+		for (auto q : mp) {
+			if (q.first != p[i]) {
 				mp[q.first]++;
 			}
 		}
 	}
-	cout<<"Process ";
-	for(i=0;i<pSize;i++){
-		cout<<p[i]<<" ";
-	}
-	cout<<endl;
-	for(i=0;i<cSize;i++){
-		cout<<"Frame "<<i<<" ";
-		for(j=0;j<pSize;j++){
-			if(a[i][j]==-1)
-				cout<<"E ";
-				else
-			cout<<a[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-	cout<<"HIT     ";
-	for(i=0;i<pSize;i++){
-		if(hi[i]==0)
-		cout<<"  ";
-		else
-		cout<<hi[i]<<" ";
-	}
-	cout<<endl;
-	cout<<"Page Hit "<<hit<<endl<<"Page Fault "<<pSize-hit<< endl;
+	cout << "Page Hit " << hit << endl << "Page Fault " << m - hit << endl;
 	return 0;
 }
